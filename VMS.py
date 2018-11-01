81751542679e55674b592ad9f8d02460190909b6
Paul Hartman <hartman.paul.dean@gmail.com>
2018-11-01 00:18:38 -0500

    Update VMS.py
    New program. Starting to build skeleton.

---
+++ VMS.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+
+# Vehicle Management Software
+# By Paul Hartman
+# Copyright 2018
+
+# What vehicle are we playing with today?
+
+# Open that vehicles file or create it if it doesn't exist
+
+# Time for some status checks on the vehicleself.
+
+# 
