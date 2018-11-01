66327a9eeb46d6c6a888c336452a545fea7fe69f
Paul Hartman <hartman.paul.dean@gmail.com>
2018-11-01 01:21:35 -0500

    Update VMS.py
    continuing to build up skeleton. Starting to add some code.

--- VMS.py
+++ VMS.py
@@ -4,10 +4,36 @@
 # By Paul Hartman
 # Copyright 2018
 
+
+# Import space
+import os
+
+# Variable space
+v = "~/VMS/vehicles"
+
+# Nice into screen (Take any input to go to next program)
+
+    #Include a manual page?
+
 # What vehicle are we playing with today?
+VIN = raw_imput("What is the VIN Number")
 
-# Open that vehicles file or create it if it doesn't exist
+# Open that vehicle's file or create it if it doesn't exist
 
-# Time for some status checks on the vehicleself.
+    #does the path exist?
 
-# 
+    #does the file exist?
+
+# Time for some status checks on the vehicle.
+
+    # Is the insurance up to date
+
+    # Is the next PM due
+
+    # Anything else?
+
+# What are we doing today? (Options menu?)
+
+# Did we use any parts?
+
+        # Any other parts?
