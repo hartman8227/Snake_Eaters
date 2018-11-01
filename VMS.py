68f23053c7bc41fa0def8232a52562e459af8885
Paul Hartman <hartman.paul.dean@gmail.com>
2018-11-01 04:07:22 -0500

    Update VMS.py
    More code updates. changed to a repair log. need to change name of file.

--- VMS.py
+++ VMS.py
@@ -1,38 +1,55 @@
 #! /usr/bin/python
 
-# Vehicle Management Software
+# Repair log for VMS
 # By Paul Hartman
 # Copyright 2018
+
+# use python 3
+
+#### DO ONE THING BUT DO IT WELL! ####
 
 
 # Import space
 import os
 
+# Def Space
+def clear():
+    if (os.name == 'nt'):
+        c = os.system('cls')
+    else:
+        c = os.system('clear')
+    del c
+
 # Variable space
-v = "~/VMS/vehicles"
-
-# Nice into screen (Take any input to go to next program)
-
-    #Include a manual page?
+path_v = "~/VMS/vehicles/"
+file_full ='path_v' + 'VIN'
 
 # What vehicle are we playing with today?
-VIN = raw_imput("What is the VIN Number")
+clear
+while false
+    VIN = raw_input("What is the VIN Number. (Type "x" to exit")
+    VIN2 = raw_input("Please confirm the VIN Number (Type "x" to exit")
 
+    if VIN2 == VIN
+        break
+    else;
+        print ("The Vin Numbers do not match! please try again. Type "x" both times to exit program.")
+
+# Escape sequence
+if VIN == "x"
+    exit(
+
+    )
 # Open that vehicle's file or create it if it doesn't exist
+try:
+    with open(file_full) as file:
+        pass
+except IOError as e:
+    print "Unable to open the file on that vehicle. Have you created it yet?"
+# NOTE: Build a program to create files on new Vehicles.
+print "please discrible the problem"
+problem = raw_input()
 
-    #does the path exist?
-
-    #does the file exist?
-
-# Time for some status checks on the vehicle.
-
-    # Is the insurance up to date
-
-    # Is the next PM due
-
-    # Anything else?
-
-# What are we doing today? (Options menu?)
 
 # Did we use any parts?
 
